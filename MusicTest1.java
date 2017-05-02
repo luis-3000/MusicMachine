e30d041f3b9607d6a73ddb463e980257cf087e29
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-01 22:16:24 -0500

    Fixed the previous problem. The program needed to make sure to handle exceptions that could be caused by the creation of the Sequencer object requested from the MidiSystem. I have included the exception handling code required to handle any possible exceptions that could be thrown.

--- MusicTest1.java
+++ MusicTest1.java
@@ -5,14 +5,20 @@ import javax.sound.midi.*; // the sound package
 public class MusicTest1 {
 
 	public void play() {
-		Sequencer sequencer = MidiSystem.getSequencer();
 
-		System.out.println("We got a sequencer");
+		// Wrapping the risky Sequencer object request in a try/catch block to handle potential problems
+		try {  
+			Sequencer sequencer = MidiSystem.getSequencer(); // Ask the MidiSystem for a Sequencer object
 
+			System.out.println("We got a sequencer");
+		} catch (MidiUnavailableException ex) {
+			System.out.println("Sequencer object not available due to resource restrictions." +
+				                         "This could mean the Sequencer object is already in use.");
+		}
 	}
 
 	public static void main(String[] args) {
-		MusicTest1 mt = new MusicTest1(); // Ask the MidiSystem for a Sequencer object
+		MusicTest1 mt = new MusicTest1(); 
 		mt.play();
 	}
 
