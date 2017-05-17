2153bef5fe632b569c7f4df990eb820de7400fe6
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-16 22:29:12 -0500

    Trying different things witht eh paintComponent() method

--- MyDrawPanel.java
+++ MyDrawPanel.java
@@ -8,7 +8,10 @@ class MyDrawPanel extends JPanel {
 	/* The system calls the Graphics method which is a drawing surface of type Graphics
 	 * that can be used to paint on it. */
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.orange);
-		g.fillRect(20, 50, 100, 100); // shape to paint
+		//g.setColor(Color.orange);
+		//g.fillRect(20, 50, 100, 100); // shape to paint
+
+		Image image = new ImageIcon("CatPicture.jpg").getImage();
+		g.drawImage(image,3,4,this);
 	}
 }
