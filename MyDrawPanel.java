087d456bfce01e39aafdbabc0de2638247b6f0f2
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-16 22:32:42 -0500

    Drawing a rectangle to test some features

--- MyDrawPanel.java
+++ MyDrawPanel.java
@@ -11,7 +11,16 @@ class MyDrawPanel extends JPanel {
 		//g.setColor(Color.orange);
 		//g.fillRect(20, 50, 100, 100); // shape to paint
 
-		Image image = new ImageIcon("CatPicture.jpg").getImage();
-		g.drawImage(image,3,4,this);
+		//Image image = new ImageIcon("CatPicture.jpg").getImage();
+		//g.drawImage(image,3,4,this);
+
+		g.fillRect(0,0,this.getWidth(), this.getHeight());
+		int red = (int) (Math.random() * 255);
+		int green = (int) (Math.random() * 255);
+		int blue = (int) (Math.random() * 255);
+
+		Color randomColor = new Color(red, green, blue);
+		g.setColor(randomColor);
+		g.fillOval(70,70,100,100);
 	}
 }
