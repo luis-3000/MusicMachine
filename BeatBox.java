e59325d9b9f0c720eed47f7a8ba3be3547a928da
Jose Luis Castillo <josecastillocs@gmail.com>
2017-08-19 21:37:21 -0500

    Added code to restore a BeatBox pattern.

--- BeatBox.java
+++ BeatBox.java
@@ -219,4 +219,30 @@ public class BeatBox {
 			}
 		}
 	}
+
+	/* Code to restore a BeacBox pattern. */
+	public class MyReadInListener implements ActionListener {
+		public void actionPerformed(ActionEvent a) {
+			boolean[] checkboxState = null;
+			try {
+				FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
+				ObjectInputStream is = new ObjectInputStream(fileIn);
+				checkboxState = (boolean[]) is.readObject();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			for(int i = 0; i < 256; i++) {
+				JCheckBox check = (JCheckBox) checkboxList.get(i);
+				if(checkboxState[i]) {
+					check.setSelected(true);
+				} else {
+					check.setSelected(false);
+				}
+			}
+
+			sequencer.stop();
+			buildTrackAndStart();
+		}
+	}
 }
